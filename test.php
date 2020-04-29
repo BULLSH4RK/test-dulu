272be7fa483b5f631912ca2f113d6892d2210c88
aditya2098 <aditya190819@gmail.com>
2020-04-29 11:36:54 +0700

    nambah test,php

---
+++ test.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>test git remote</title>
+</head>
+<body>
+    <h1>halo dunia</h1>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas ex libero amet fugit in sunt voluptatum rem obcaecati praesentium culpa accusantium, ea, dolorum quis sapiente illo doloremque, et autem dolores?</p>
+</body>
+</html>
